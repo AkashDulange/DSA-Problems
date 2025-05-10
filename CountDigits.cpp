7b9c32fe7cbbec4cf6b717ca59082bc387c0e63f
Akash <aakashdulange2004@gmail.com>
2025-05-10 12:59:55 +0530

    Added CountDigits.cpp - Simple problem to count number of digits that divide the number evenly.

---
+++ CountDigits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    // Function to count the number of digits in n that evenly divide n
+    int evenlyDivides(int n) {
+        // code here
+        int count=0;
+        int  num = n;
+        while(n!=0){
+           int digit = n%10;
+           if(digit!=0 && num%digit==0){
+               count++;
+           }
+           n = n/10;
+        }
+        return count;
+        
+    }
+};
